9fc473dd1775cbdb503beb23cf4246c510f6116a
abhik <abhiksahu2003@gmail.com>
2024-04-20 17:27:13 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   const [color, setcolor] = useState("black")
 
   return (
+        /*we used this type of expression on onclick because onclick exprects a function but when we pass a function with arguments it returns some value which we dont want so we used arrow function to return the value */ 
     <>
       <div className='w-full h-screen flex justify-center items-center' style={{ backgroundColor: color }}></div>
 
       <div className='fixed flex justify-center items-center bottom-1/2 w-full inset-x-0  px-2'>
         <div className='flex flex-wrap justify-center gap-4 shadow-xl px-3 py-3 rounded-3xl bg-gray-50'>
-        //we used this type of expression on onclick because onclick exprects a function but when we pass a function with arguments it returns some value which we dont want so we used arrow function to return the value  
         <button className='outline-none px-4 py-1 rounded-full shadow-lg' onClick={(set)=>setcolor("red")} style={{backgroundColor:"red"}}>RED</button>
         <button className='outline-none px-4 py-1 rounded-full shadow-lg' onClick={(set)=>setcolor("green")} style={{backgroundColor:"green"}}>GREEN</button>
         <button className='outline-none px-4 py-1 rounded-full shadow-lg' onClick={(set)=>setcolor("blue")} style={{backgroundColor:"blue"}}>BLUE</button>
